7443e64a55b8619c7b6b1313e3bcc629aafd5cee
DolosMars <high106huiyu@gmail.com>
2022-05-04 11:00:47 +0800

    Initial Commit

---
+++ finalDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  finalDemo
+//
+//  Created by User06 on 2022/5/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ finalDemo/finalDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalDemoApp.swift
+//  finalDemo
+//
+//  Created by User06 on 2022/5/4.
+//
+
+import SwiftUI
+
+@main
+struct finalDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
